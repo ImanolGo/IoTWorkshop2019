716a2c2574fdd4ece20075b470ae06b9e2315e32
Imanol Gomez <yo@imanolgomez.net>
2019-01-06 16:13:13 +0000

    added files

---
+++ blynk/PushDataESP8266/PushDataESP8266.ino
@@ -0,0 +1,43 @@
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+// Use Virtual pin 5 for uptime display
+#define PIN_UPTIME V5
+
+// This function tells Arduino what to do if there is a Widget
+// which is requesting data for Virtual Pin (5)
+BLYNK_READ(PIN_UPTIME)
+{
+  // This command writes Arduino's uptime in seconds to Virtual Pin (5)
+  Blynk.virtualWrite(PIN_UPTIME, millis() / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+}
